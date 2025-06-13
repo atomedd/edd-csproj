6c4a7bb05c00169f812483068433c160bc6d09c1
atomedd <edmar.viannas@gmail.com>
2025-06-12 19:07:53 -0700

    Changed Port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3165;
 
 
 //AUTH OFF FOR NOW
