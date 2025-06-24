0c15ac3a34e8a4861b440374eae6d9140553adbc
atomedd <edmar.viannas@gmail.com>
2025-06-23 21:52:49 -0700

    update users.js and auth.js

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  steamId: String,
-  xboxId: String,
-  psnId: String,
-  friends: [String], // List of user IDs
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  steamId: { type: String, default: '' },
+  xboxId: { type: String, default: '' },
+  psnId: { type: String, default: '' },
+  friends: [String],
   preferences: Object,
 });
 
