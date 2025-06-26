3aa0ad6e4e9984b59661a5e46cd8e74200e751c7
atomedd <edmar.viannas@gmail.com>
2025-06-25 19:13:48 -0700

    update steam.js

--- backend/services/steam.js
+++ backend/services/steam.js
@@ -2,14 +2,19 @@
 
   const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
+  if (!STEAM_API_KEY) {
+  console.error('Steam API key not loaded. Check your .env file.');
+}
+
   const getPlayerSummaries = async (steamId) => {
-    const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${steamId}`;
+    const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${steamId}`;
     const response = await axios.get(url);
     return response.data.response.players[0];
   };
+  
 
   const getOwnedGames = async (steamId) => {
-    const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamId}&include_appinfo=true&include_played_free_games=true`;
+    const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamId}&include_appinfo=true&include_played_free_games=true`;
     const response = await axios.get(url);
     return response.data.response.games || [];
   };
@@ -21,7 +26,7 @@
   };
 
   const getRecentActivity = async (steamId) => {
-    const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamId}`;
+    const url = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=${STEAM_API_KEY}&steamid=${steamId}`;
     const response = await axios.get(url);
     return response.data.response.games || [];
   };
