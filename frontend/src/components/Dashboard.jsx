bdb9ff4fcbafaa00e77a8f91ed63a36888bac306
atomedd <edmar.viannas@gmail.com>
2025-06-26 23:05:01 -0700

    update frontend/public/index.html and Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -47,7 +47,6 @@ export default function Dashboard() {
 
           <p className="text-gray-700">
             🎮 Total Hours Played: {(overview.totalPlaytime / 60).toFixed(1)}
-            <h1 className="text-3xl font-bold text-purple-600">Hello, Tailwind!</h1>
 
           </p>
         </div>
