3ff1afce157d16cfe20682f7afe7dd92ed74782d
atomedd <edmar.viannas@gmail.com>
2025-06-26 22:08:11 -0700

    update App.js, Login.jsx and ProtectedRoute.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,44 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import OwnedGames from "./components/OwnedGames";
 import Profile from "./components/Profile";
-import SteamRedirectHandler from "./components/SteamRedirectHandler"; 
+
+
+
+
+
 
 function App() {
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    const token = query.get("token");
+    if (token) {
+      localStorage.setItem("token", token);
+      // Remove token from URL to clean it up
+      window.history.replaceState(null, "", "/dashboard");
+    }
+  }, []);
+
+
   return (
     <div className="min-h-screen bg-gray-100">
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route element={<ProtectedRoute />}>
+          
+          <Route element={<ProtectedRoute />}> {/* PROTECTED ROUTES */}
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/games" element={<OwnedGames />} />
             <Route path="/profile" element={<Profile />} />
+
           </Route>
-
-
-          <Route path="/" element={<SteamRedirectHandler />} />
+          {/* DEFAUTL CALLBACK */}
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
