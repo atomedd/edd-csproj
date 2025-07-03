21660887e1b8d68be6d6b62d01e2cd43d8cbbe48
atomedd <edmar.viannas@gmail.com>
2025-07-03 02:33:57 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,40 +4,41 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
-const steamRoutes = require('./routes/steam');
-const authRoutes = require('./routes/auth');
 const session = require('express-session');
 const passport = require('passport');
 require('./config/passport');
 
-
+const steamRoutes = require('./routes/steam');
+const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 
 const app = express();
-app.use('/users', userRoutes);
 const PORT = process.env.PORT || 3165;
 
-// Middleware
+// MIDDLEWARE
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
-app.use(session({ secret: process.env.SESSION_SECRET || 'supersecretkey', resave: false, saveUninitialized: false, cookie: { secure: false }}));
-
-//STEAM STUFF
-app.use('/api/steam', steamRoutes);
+app.use(session({ 
+  secret: process.env.SESSION_SECRET || 'supersecretkey', 
+  resave: false, 
+  saveUninitialized: false, 
+  cookie: { secure: false }
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// STEAM STUFF
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/steam', steamRoutes);
 
+// Test route
+app.get('/', (req, res) => res.send('API Running'));
 
-// MongoDB Connection
+// MONGODB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('Database connection error:', err));
 
-// Route Middleware
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.get('/', (req, res) => res.send('API Running'));
-
-// Start Server
+// SERVER RUNNING
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
