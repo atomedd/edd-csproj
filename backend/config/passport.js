1abc1995478d62f797a427e6d50ba47fdc07fd16
atomedd <edmar.viannas@gmail.com>
2025-07-03 17:28:42 -0700

    update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -16,26 +16,37 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-passport.use(new SteamStrategy({
-  returnURL: process.env.STEAM_RETURN_URL,
-  realm: process.env.STEAM_REALM,
-  apiKey: process.env.STEAM_API_KEY
-}, async (identifier, profile, done) => {
-  try {
-    const steamId = profile.id;
-    let user = await User.findOne({ steamId });
+passport.use(new SteamStrategy(
+  {
+    returnURL: process.env.STEAM_RETURN_URL,
+    realm: process.env.STEAM_REALM,
+    apiKey: process.env.STEAM_API_KEY
+  },
+  async (identifier, profile, done) => {
+    try {
+      const steamId = profile.id;
+      let user = await User.findOne({ steamId });
 
-    if (!user) {
-      user = await User.create({
-        username: profile.displayName,
-        steamId,
-        password: '',
-        authProvider: 'steam'
-      });
+      if (!user) {
+        let baseUsername = profile.displayName || `SteamUser_${steamId.slice(-5)}`;
+        let uniqueUsername = baseUsername;
+        let suffix = 1;
+
+        while (await User.findOne({ username: uniqueUsername })) {
+          uniqueUsername = `${baseUsername}_${suffix++}`;
+        }
+
+        user = await User.create({
+          username: uniqueUsername,
+          steamId,
+          password: '',
+          authProvider: 'steam'
+        });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
-
-    return done(null, user);
-  } catch (err) {
-    return done(err);
   }
-}));
+));
