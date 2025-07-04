67e27b26851d33a13b6cc083b6a8df616b96c66b
atomedd <edmar.viannas@gmail.com>
2025-07-03 17:14:15 -0700

    update passport.js and User.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,7 +3,6 @@ const passport = require('passport');
 const SteamStrategy = require('passport-steam').Strategy;
 const User = require('../models/User');
 
-// SERIALIZE/ DESERIALIZE
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -17,7 +16,6 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-// STEAM STRATEGY
 passport.use(new SteamStrategy({
   returnURL: process.env.STEAM_RETURN_URL,
   realm: process.env.STEAM_REALM,
@@ -25,26 +23,20 @@ passport.use(new SteamStrategy({
 }, async (identifier, profile, done) => {
   try {
     const steamId = profile.id;
-
-    // CHECK IF USER EXISTS
     let user = await User.findOne({ steamId });
 
-    // IF NOT MAKE A NEW ONE
     if (!user) {
       let baseUsername = profile.displayName;
       let uniqueUsername = baseUsername;
       let suffix = 1;
 
-      // ENSURE UNIQUE USERNAME
       while (await User.findOne({ username: uniqueUsername })) {
         uniqueUsername = `${baseUsername}_${suffix++}`;
       }
 
-      // CREATE NEW USER
       user = await User.create({
         username: uniqueUsername,
         steamId,
-        email: null,             
         password: '',
         authProvider: 'steam'
       });
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: false, unique: true, sparse: true },
+  email: {
+    type: String,
+    required: false,
+    unique: true,
+    sparse: true
+  },
   password: {
     type: String,
     required: function () {
-      // WONT ASK FOR PASSWORD IF LOGGING IN THRU STEAM/XBOX/PLAYSTATION
       return !this.steamId && !this.xboxId && !this.psnId;
     },
   },
   steamId: { type: String, default: '' },
-  xboxId: { type: String, default: '' },  
-  psnId: { type: String, default: '' },   
+  xboxId: { type: String, default: '' },
+  psnId: { type: String, default: '' },
   friends: [String],
   preferences: Object,
 });
